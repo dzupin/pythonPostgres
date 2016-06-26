490e0acb2e9a1b28bd327f716f077f311453d6b6
dzupin <dzupinr@gmail.com>
2016-06-26 17:56:46 +0200

    initial commit to access table in local Postgres database

---
+++ localPostgresTable.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='cemdb' user='postgres' host='localhost' password='postgres'")
+    print "Connected to local database"
+except:
+    print "I am unable to connect to local Postgres database"
+
+cur = conn.cursor()
+cur.execute("SELECT * from mytesttable")
+
+rows = cur.fetchall()
+print "\nShow me the some data:\n"
+for row in rows:
+    print  "id: ",row[0]," first:\t", row[1], " second:\t", row[2], " size:\t", row[3]

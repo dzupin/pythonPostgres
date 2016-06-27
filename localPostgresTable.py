02d2d0db95cc827534b8ad658caa29f0b8d2bb73
dzupin <dzupinr@gmail.com>
2016-06-27 11:19:24 +0200

    updated connection string

--- localPostgresTable.py
+++ localPostgresTable.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 try:
-    conn = psycopg2.connect("dbname='cemdb' user='postgres' host='localhost' password='postgres'")
+    conn = psycopg2.connect(dbname='cemdb', user='postgres', host='localhost', password='postgres', port='5432' )
     print "Connected to local database"
 except:
     print "I am unable to connect to local Postgres database"

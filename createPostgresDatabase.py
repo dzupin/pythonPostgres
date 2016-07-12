87f529efe18e0032b88ac602b396cb1002eedd26
dzuro02 <Robert.Dzupin@ca.com>
2016-07-12 11:54:18 +0200

    added code to create and drop database

---
+++ createPostgresDatabase.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+Host='192.168.86.188'
+User='postgres'
+Password='postgres'
+Dbname='postgres'  #Use default installation database for initial connection
+DbnameNew='mydatabase'
+Port='5432'
+
+try:
+    conn = psycopg2.connect(dbname=Dbname, user=User, host=Host, password=Password, port=Port )
+    print "Connected to default database of Postgres server"
+except:
+    print "I am unable to connect to Postgres server"
+
+
+try:
+    CreateStatement = "CREATE DATABASE " + DbnameNew
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) #to create database you must overwrite default (ISOLATION_LEVEL_READ_COMMITTED) isolation level
+    cur = conn.cursor()
+    cur.execute(CreateStatement)
+    print "Successfully created new database: " + DbnameNew
+except:
+    print "Failed to create new Postgres database"
+
+
+cur.close()
+conn.close()
---
+++ dropPostgresDatabase.py
@@ -0,0 +1,27 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+Host='192.168.86.188'
+User='postgres'
+Password='postgres'
+Dbname='postgres'  #Use default installation database for initial connection
+DbnameOld='mydatabase'  #In order to drop specific database your connection string must use different instance
+Port='5432'
+
+try:
+    conn = psycopg2.connect(dbname=Dbname, user=User, host=Host, password=Password, port=Port )
+    print "Connected to default database of Postgres server"
+except:
+    print "I am unable to connect to Postgres server"
+
+try:
+    DropDatabaseStatement = "DROP DATABASE " + DbnameOld
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # to drop database you must overwrite default (ISOLATION_LEVEL_READ_COMMITTED) isolation level
+    cur = conn.cursor()
+    cur.execute(DropDatabaseStatement)
+    print "Successfully removed/dropped database: " + DbnameOld
+except:
+    print "Failed to delete Postgres database"
+
+
+cur.close()
+conn.close()

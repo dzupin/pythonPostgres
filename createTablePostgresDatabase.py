de84334c8c657bdf03819ab8a291adccbb6ed66e
dzuro02 <Robert.Dzupin@ca.com>
2016-07-14 12:59:05 +0200

    Set unicode encoding to parser properly process non-ascii

---
+++ createTablePostgresDatabase.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import psycopg2
+import sys
+Host='192.168.86.189'
+User='postgres'
+Password='postgres'
+Dbname='mydatabase'
+Port='5432'
+
+conn = None
+try:
+    conn = psycopg2.connect(dbname=Dbname, user=User, host=Host, password=Password, port=Port )
+    print "Connected to following database on Postgres server: " + Dbname
+except:
+    print "I am unable to connect to Postgres server"
+
+try:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE Cars(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Price INT)")
+    cur.execute("INSERT INTO Cars VALUES(1,'Audi',52642)")
+    cur.execute("INSERT INTO Cars VALUES(2,'Mercedes',57127)")
+    cur.execute("INSERT INTO Cars VALUES(3,'Skoda',9000)")
+    cur.execute("INSERT INTO Cars VALUES(4,'Volvo',29000)")
+    cur.execute("INSERT INTO Cars VALUES(5,'Bentley',350000)")
+    cur.execute("INSERT INTO Cars VALUES(6,'Citroen',21000)")
+    cur.execute("INSERT INTO Cars VALUES(7,'Hummer',41400)")
+    cur.execute("INSERT INTO Cars VALUES(8,'Volkswagen',21600)")
+    conn.commit()
+    print "Successfully created new table with sample data"
+
+except psycopg2.DatabaseError, e:
+    if conn:
+        conn.rollback()
+    print 'Error %s' % e
+    print "Failed to create new table in your Postgres database"
+    sys.exit(1)
+
+finally:
+    if conn:
+        conn.close()
+
+
+conn.close()
